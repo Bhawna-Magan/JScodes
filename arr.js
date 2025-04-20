26e1b45aa83e227341d43c7a96fb13c0f657933f
Bhawna <bhawnamagan@gmail.com>
2025-04-21 00:34:02 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,29 @@
+// Array 
+// array is collection of item
+// Array is mutable that means it can change
+// let arr=[1,2,4,7];
+// console.log(arr);
+// console.log(arr.length);
+// console.log(typeof arr); 
+// let sports = ["Basketball","Footbal","Hockey","Handball","Kho-Kho"];
+// console.log(sports);
+// console.log(sports[2]); //indices
+// sports[3]="Jubo";
+// console.log(sports);
+
+// loops over a array
+
+// for loop
+let sports = ["Basketball","Footbal","Hockey","Handball","Kho-Kho"];
+
+// for(i=0;i<sports.length;i++){
+//     console.log(sports[i]);
+// }
+
+// for of loops
+for(let game of sports){
+    console.log(`game=${game}`);
+}
+
+
+// DATE = 20-04-2025
